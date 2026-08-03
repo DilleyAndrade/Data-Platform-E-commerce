df47eb2ef3ecf73b9c6f50cfb16bacfdee6c582e
Dilley Andrade <dilleyandrade@gmail.com>
2026-08-03 19:42:14 -0300

    feat: added postgre ingestion

---
+++ src/ingestion/ingestion_postgres.py
@@ -0,0 +1,66 @@
+import os
+import io
+import gc
+import boto3
+import pandas as pd
+from datetime import date
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+PG_HOST = os.getenv("PG_HOST")
+PG_PORT = os.getenv("PG_PORT")
+PG_DB = os.getenv("PG_DB")
+PG_USER = os.getenv("PG_USER")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+
+tables = ["customers", "products", "suppliers"]
+
+def ingestion_postgres():
+
+  engine = create_engine(f"postgresql+psycopg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}")
+
+  def get_s3_client():
+    endpoint = os.getenv("AWS_ENDPOINT_URL")
+    return boto3.client(
+        "s3",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        endpoint_url=endpoint if endpoint else None
+    )
+  
+  s3 = get_s3_client()
+
+
+  def create_dataframe(query):
+    df = pd.read_sql(query, engine)
+
+    print(df.head())
+    return df
+
+
+  def save_postgres_file(data_frame, table_name):
+    try:
+      bucket_name = "landing"
+      key = f"{table_name}/ingestion_date_{date.today().strftime('%Y%m%d')}/{table_name}.parquet"
+
+      with io.BytesIO() as buffer:
+        data_frame.to_parquet(buffer, index=False)
+        s3.put_object(
+            Bucket=bucket_name,
+            Key=key,
+            Body=buffer.getvalue()
+        )
+      del data_frame
+      gc.collect()
+
+      print(f"File {table_name} saved!")
+
+    except Exception as e:
+        print(f"Error to save file. Error: {e}")
+
+
+  for table in tables:
+    query = f"select * from public.{table};"
+    save_postgres_file(create_dataframe(query), table)
