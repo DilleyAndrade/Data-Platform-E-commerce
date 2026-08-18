c23a0b1500207d9ffe9d2cca87637ba759605658
Dilley Andrade <dilleyandrade@gmail.com>
2026-08-17 22:04:03 -0300

    update: added ingestion local executor

--- src/run_all_pipeline.py
+++ src/run_all_pipeline.py
@@ -1,7 +1,9 @@
 from ingestion.ingestion_api import ingestion_api
+from ingestion.ingestion_local import ingestion_local
 from ingestion.ingestion_mysql import ingestion_mysql
 from ingestion.ingestion_postgres import ingestion_postgres
 
-ingestion_api()
-ingestion_postgres()
-ingestion_mysql()
+# ingestion_api()
+# ingestion_postgres()
+# ingestion_mysql()
+ingestion_local()
