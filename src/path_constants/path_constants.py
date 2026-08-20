ae26b3d174c5f30a6c6f3b04396c0fd3a739a9d0
Dilley Andrade <dilleyandrade@gmail.com>
2026-08-20 10:25:54 -0300

    update: added buckets variables

--- src/path_constants/path_constants.py
+++ src/path_constants/path_constants.py
@@ -2,3 +2,17 @@
 URL_CUSTOMER_REVIEW =     "http://localhost:8000/customer-reviews"
 URL_EXCHANGE_RATES =      "http://localhost:8000/exchange-rates"
 URL_MARKETING_CAMPAIGNS = "http://localhost:8000/marketing-campaigns"
+
+#local files
+PATH_LOCAL_FILES = "C:/Users/dille/OneDrive/Desktop/data-platform/local_data_source"
+
+#buckets
+
+BUCKET_LAN = "landing"
+BUCKET_RAW = "raw"
+BUCKET_BRO = "bronze"
+BUCKET_SIL = "silver"
+BUCKET_GOL = "gold"
+BUCKET_OBS = "observability"
+BUCKET_REJ = "rejected"
+BUCKET_QUA = "quarantine"
