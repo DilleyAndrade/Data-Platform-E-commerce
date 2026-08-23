703c1a7d7b6240d2dbc942fbfdab09264712b5ed
Dilley Andrade <dilleyandrade@gmail.com>
2026-08-22 22:34:11 -0300

    update: added mysql/api ingestion

--- src/run_all_pipeline.py
+++ src/run_all_pipeline.py
@@ -1,8 +1,20 @@
+from ingestion.ingestion_api import ingestion_api
 from ingestion.ingestion_local import ingestion_local
+from ingestion.ingestion_mysql import ingestion_mysql
+from ingestion.ingestion_postgres import ingestion_postgres
 from utils.spark_session import spark_session
 
 spark = spark_session("ingestion_pipeline", "local[*]")
 
 ingestion_local(spark)
+ingestion_postgres(spark)
+ingestion_mysql(spark)
+ingestion_api(spark)
+
+
+s3_path = f"s3a://observability/ingestion_log"
+
+df_log = spark.read.format("delta").load(s3_path)
+df_log.show(100)
 
 spark.stop()
