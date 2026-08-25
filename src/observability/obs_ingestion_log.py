01ff710f483f6d6263cdd776361a65e4275a42e6
Dilley Andrade <dilleyandrade@gmail.com>
2026-08-25 09:10:04 -0300

    feat: added ingestion log

--- src/observability/obs_ingestion_log.py
+++ src/observability/obs_ingestion_log.py
@@ -1,54 +1,29 @@
-from uuid import uuid4
 from datetime import date
-from schemas.schemas import ingestion_log_schema
 from path_constants.path_constants import BUCKET_OBS
+from schemas.schemas import ingestion_log_schema
 
-execution_id =f"ingestion_{ uuid4()}"
 
-def create_ingestion_log_table(
-  spark,
-  source_name,
-  source_table,
-  source_type,
-  file_name,
-  target_path,
-  started_at,
-  ended_at,
-  execution_status,
-  error_message
-):
-
-  ingestion_log_columns = [
-    {
-      "execution_id": execution_id,
-      "source_name": source_name,
-      "source_table": source_table,
-      "source_type": source_type,
-      "file_name": file_name,
-      "target_path": target_path,
-      "started_at": started_at,
-      "ended_at": ended_at,
-      "duration_seconds": int((ended_at-started_at).total_seconds()),
-      "execution_status": execution_status,
-      "error_message": error_message,
-      "execution_date": date.today()
+def create_ingestion_log(run_id, source_name, source_table, source_type, file_name,
+                         target_path, started_at, ended_at, status, error_message):
+    return {
+        "run_id": run_id,
+        "source_name": source_name,
+        "source_table": source_table,
+        "source_type": source_type,
+        "file_name": file_name,
+        "target_path": target_path,
+        "started_at": started_at,
+        "ended_at": ended_at,
+        "duration_seconds": (ended_at - started_at).total_seconds(),
+        "execution_status": status,
+        "error_message": error_message,
+        "execution_date": date.today(),
     }
-  ]
 
-  df_ingestion_log = spark.createDataFrame(
-    ingestion_log_columns,
-    schema=ingestion_log_schema
-  )
 
-  s3_path = f"s3a://{BUCKET_OBS}/ingestion_log"
-  
-  df_ingestion_log.coalesce(1).write \
-    .format("delta") \
-    .mode("append") \
-    .save(s3_path)
-
-  spark.sql(f"OPTIMIZE delta.`{s3_path}`")
-
-  spark.sql(f"VACUUM delta.`{s3_path}` RETAIN 178 HOURS")
- 
-  return df_ingestion_log
+def write_ingestion_log(spark, logs):
+    if not logs:
+        return None
+    dataframe = spark.createDataFrame(logs, schema=ingestion_log_schema)
+    dataframe.write.format("delta").mode("append").save(f"s3a://{BUCKET_OBS}/ingestion_log")
+    return dataframe
