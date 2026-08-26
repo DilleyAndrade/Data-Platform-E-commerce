c378c9fd0605412d623877e37a69d44bcaee7a99
Dilley Andrade <dilleyandrade@gmail.com>
2026-08-25 22:29:46 -0300

    perf: configure Spark Delta optimizations

--- src/utils/spark_session.py
+++ src/utils/spark_session.py
@@ -10,15 +10,13 @@ SPARK_PACKAGES = ",".join(
         "org.postgresql:postgresql:42.7.3",
     )
 )
-
-
 def spark_session(app_name: str, master_mode: str):
     warehouse_directory = os.getenv(
         "SPARK_WAREHOUSE_DIR",
         DEFAULT_SPARK_WAREHOUSE,
     )
 
-    return (
+    spark = (
         SparkSession.builder
         .appName(app_name)
         .master(master_mode)
@@ -36,6 +34,28 @@ def spark_session(app_name: str, master_mode: str):
         .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID"))
         .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY"))
         .config("spark.hadoop.fs.s3a.path.style.access", "true")
+        .config("spark.hadoop.fs.s3a.ssl.channel.mode", "default_jsse")
         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        .config("spark.databricks.delta.optimizeWrite.enabled", "true")
+        .config("spark.databricks.delta.autoCompact.enabled", "true")
+        .config(
+            "spark.databricks.delta.autoCompact.maxFileSize",
+            str(128 * 1024 * 1024),
+        )
         .getOrCreate()
     )
+    _suppress_windows_temp_cleanup_warning(spark)
+    return spark
+
+
+def _suppress_windows_temp_cleanup_warning(spark):
+    if os.name != "nt":
+        return
+
+    try:
+        log_manager = spark.sparkContext._jvm.org.apache.logging.log4j.LogManager
+        level = spark.sparkContext._jvm.org.apache.logging.log4j.Level
+        log_manager.getLogger("org.apache.spark.SparkEnv").setLevel(level.ERROR)
+    except Exception:
+        # A configuracao do logger nao deve impedir a criacao da sessao Spark.
+        return
